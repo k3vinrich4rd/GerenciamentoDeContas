9e18ff2a954eb1f5df65833b1e27ae50bc2a2632
k3vinrich4rd <kevinrichardve@gmail.com>
2022-08-24 14:13:48 -0300

    Inserindo escopo do projeto

---
+++ src/test/java/com/example/gerenciamentoDeContas/GerenciamentoDeContasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamentoDeContas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
