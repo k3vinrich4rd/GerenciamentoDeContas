6d9a8f080bc7226d695a610cb5e628efc3467bb3
k3vinrich4rd <kevinrichardve@gmail.com>
2022-09-13 17:25:48 -0300

    Inserindo atualizações de formato

--- src/main/java/com/example/gerenciamentoDeContas/service/ContasAReceberService.java
+++ src/main/java/com/example/gerenciamentoDeContas/service/ContasAReceberService.java
@@ -1,12 +1,17 @@
 package com.example.gerenciamentoDeContas.service;
 
+import com.example.gerenciamentoDeContas.enumeric.RecebimentoAlugueis;
 import com.example.gerenciamentoDeContas.model.ContasReceberModel;
 import com.example.gerenciamentoDeContas.repository.IContasAReceberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.Enumerated;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+
+import static com.example.gerenciamentoDeContas.enumeric.RecebimentoAlugueis.*;
 
 @Service
 public class ContasAReceberService {
@@ -16,10 +21,18 @@ public class ContasAReceberService {
     private IContasAReceberRepository iContasAReceberRepository;
 
     public ContasReceberModel cadastrarNovoRecebimento(ContasReceberModel contasReceberModel) {
+        Boolean recebimentoEmDia = LocalDate.now().isBefore(contasReceberModel.getDataDeVencimento()) || LocalDate.now().equals(contasReceberModel.getDataDeVencimento());
+        if (Boolean.FALSE.equals(recebimentoEmDia)) {
+            contasReceberModel.setRecebimentoAlugueis(EM_ATRASO) ;
+        } else if (Boolean.TRUE.equals(recebimentoEmDia)) {
+            contasReceberModel.setRecebimentoAlugueis(EM_DIA);
+        } else {
+            contasReceberModel.setRecebimentoAlugueis(ADIANTADO);
+        }
         return iContasAReceberRepository.save(contasReceberModel);
     }
 
-    public List<ContasReceberModel> exibirContas(){
+    public List<ContasReceberModel> exibirContas() {
         return iContasAReceberRepository.findAll();
     }
 
