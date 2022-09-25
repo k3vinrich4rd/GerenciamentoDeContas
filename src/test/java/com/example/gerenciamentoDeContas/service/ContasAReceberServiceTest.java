2261e072cca7483849b6cc0e185441a66881f0a7
k3vinrich4rd <kevinrichardve@gmail.com>
2022-09-25 17:23:43 -0300

    Inserindo criação de test 'ContasAReceberServiceTest'

--- src/test/java/com/example/gerenciamentoDeContas/service/ContasAReceberServiceTest.java
+++ src/test/java/com/example/gerenciamentoDeContas/service/ContasAReceberServiceTest.java
@@ -1,2 +1,70 @@
-package com.example.gerenciamentoDeContas.service;public class ContasAReceberServiceTest {
+package com.example.gerenciamentoDeContas.service;
+
+import com.example.gerenciamentoDeContas.model.ContasReceberModel;
+import com.example.gerenciamentoDeContas.repository.IContasAReceberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+public class ContasAReceberServiceTest {
+
+    @Mock
+    ContasAReceberService contasAReceberService;
+
+    @MockBean
+    IContasAReceberRepository iContasAReceberRepository;
+
+    private ContasReceberModel contasReceberModel;
+
+    @BeforeEach
+    private void inicializadora() {
+        MockitoAnnotations.openMocks(this);
+        contasReceberModel = new ContasReceberModel();
+    }
+
+    @Test
+    @DisplayName("Testando o metodo de cadastrar")
+    void testeCadastrarContasAReceberEstaFuncionando() {
+        iContasAReceberRepository.save(contasReceberModel);
+        Mockito.when(iContasAReceberRepository.existsById(Mockito.anyLong())).thenReturn(true);
+        Mockito.verify(iContasAReceberRepository, Mockito.times(1)).save(contasReceberModel);
+    }
+
+    @Test
+    @DisplayName("O teste deve retornar true, pois nenhuma contasAReceber foi cadastrada")
+    void testeExibirContasAReceber() {
+        List<ContasReceberModel> contasReceberModelList = contasAReceberService.exibirTodosRecebimentos();
+        Assertions.assertTrue(contasReceberModelList.isEmpty());
+    }
+
+    @Test
+    @DisplayName("O teste deve retornar true, se o metodo de buscar por id estiver funcionando corretamente")
+    void testeBuscarContasAReceberViaId() {
+        Optional<ContasReceberModel> contasReceberModelOptional = contasAReceberService.exibirRecebimentoViaId(1L);
+        Assertions.assertTrue(true, String.valueOf(contasReceberModelOptional));
+    }
+
+    @Test
+    @DisplayName("O teste deve retornar true, se o metodo de atualizar estiver funcionando corretamente")
+    void testeAtualizarContasAReceber() {
+        contasAReceberService.atualizarContas(contasReceberModel);
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("O teste deve retornar true, se o metodo deletar via id estiver funcionando corretamente")
+    void testeDeletarContasAReceberViaId() {
+        contasAReceberService.deletarContasCadastradas(1L);
+        Assertions.assertTrue(true);
+    }
 }
